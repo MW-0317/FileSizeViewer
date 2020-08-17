3df17460f11b2f66e9ea55b4eca24558d1255a8e
MW-0317 <58039651+MW-0317@users.noreply.github.com>
2020-08-16 22:10:31 -0400

    Overall increase in quality
    Features:
     - Updated sorting method, now sorts by file size and seperates folders and files
     - Added back button
    
    Bug Fixes:
     - Fixed a bug where files are 0B

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from hurry.filesize import size as sizef
 import tkinter as tk
 from tkinter import filedialog
+import tkinter.ttk as ttk
 
 class Sizr(tk.Frame):
     current_dir = "."
@@ -11,9 +12,12 @@ class Sizr(tk.Frame):
         super().__init__(master)
         self.master = master
         self.pack()
+        self.restart_window(self.gen_dialog_box())
+
+    def restart_window(self, current_dir):
         self.file_list = None
         self.size_list = None
-        self.current_dir = self.gen_dialog_box()
+        self.current_dir = current_dir
         self.size_window()
 
 
@@ -22,23 +26,24 @@ class Sizr(tk.Frame):
         for root, dirs, files in os.walk(start_path, topdown=False):
             for name in files:
                 fp = os.path.join(root, name)
-                if not os.path.islink(fp):
-                    try:
-                        size = os.path.getsize(fp)
-                        total_size += size
-                    except:
-                        pass
+                try:
+                    size = os.path.getsize(fp)
+                    total_size += size
+                except:
+                    pass
         return total_size
 
     def get_each_folder_size(self, start_path="."):
         all_files = [f for f in os.listdir(start_path)]
         files_with_sizes = []
         for file in all_files:
-            if not os.path.isfile(file):
-                size = self.get_size(os.path.join(start_path, file))
+            file_path = os.path.join(start_path, file)
+            if not os.path.isfile(file_path):
+                size = self.get_size(file_path)
                 files_with_sizes.append((file, size))
             else:
-                size = os.path.getsize(file)
+                size = os.path.getsize(file_path)
+                print(size)
                 files_with_sizes.append((file, size))
 
         return files_with_sizes
@@ -46,12 +51,23 @@ class Sizr(tk.Frame):
     def sort_files(self, files):
         def sorting_method(file):
             return file[1]
-        return files.sort(reverse=True, key=sorting_method)
+        def sorting_method_two(file):
+            return os.path.isfile(os.path.join(self.current_dir, file[0]))
+        files.sort(reverse=True, key=sorting_method)
+        files.sort(key=sorting_method_two)
+
+
+
+    def set_current_dir(self, event):
+        cs = self.file_list.curselection()
+
+        file_dir = os.path.join(self.current_dir, self.file_list.get(cs))
+        self.restart_window(file_dir)
 
     def gen_size_window_scroll(self):
         scrollbar = tk.Scrollbar(self)
         # scrollbar.pack(side = tk.RIGHT, fill = tk.Y)
-        scrollbar.grid(row=0,column=2, rowspan=3, sticky="ns")
+        scrollbar.grid(row=2,column=2, rowspan=2, sticky="ns")
 
         self.file_list = tk.Listbox(self, yscrollcommand = scrollbar.set)
         self.size_list = tk.Listbox(self, yscrollcommand = scrollbar.set)
@@ -62,10 +78,14 @@ class Sizr(tk.Frame):
             self.size_list.insert(tk.END, sizef(size))
 
         # self.file_list.pack(side=tk.TOP, fill = tk.BOTH)
-        self.file_list.grid(row=1, column=0, sticky=tk.E)
+        self.file_list.bind("<Double-1>", self.set_current_dir)
+        self.file_list.grid(row=2, column=0, sticky=tk.E)
         # self.size_list.pack(side=tk.TOP, fill = tk.BOTH)
-        self.size_list.grid(row=1, column=1, sticky=tk.E)
+        self.size_list.grid(row=2, column=1, sticky=tk.E)
         scrollbar.config(command = self.scroll)
+
+    def test_command():
+        print("test")
 
     def scroll(self, *args):
         self.file_list.yview(*args)
@@ -76,15 +96,27 @@ class Sizr(tk.Frame):
         self.label = tk.Label(self)
         self.label['text'] = left_text
         # self.label.pack(side="top")
-        self.label.grid(row=0, column=0)
+        self.label.grid(row=1, column=0)
         self.label = tk.Label(self)
         self.label['text'] = right_text
         # self.label.pack(side="top")
-        self.label.grid(row=0, column=1)
+        self.label.grid(row=1, column=1)
+
+    def back(self):
+        new_dir, end = os.path.split(self.current_dir)
+        self.restart_window(new_dir)
+
+    def create_back_button(self):
+        button = tk.Button(self)
+        button['text'] = ''
+        button['command'] = self.back
+        button.grid(row=1, column=2)
 
     def size_window(self):
+        self.create_back_button()
         self.size_window_create_label("Filename", "Size")
         self.gen_size_window_scroll()
+
 
     def gen_dialog_box(self):
         return filedialog.askdirectory()
